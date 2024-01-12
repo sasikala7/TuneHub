18a41f3ce4ba8cf59b81069c3f807fd0f016c1cd
sasikala7 <skala3873@gmail.com>
2024-01-12 15:40:11 +0530

    1st commit

---
+++ src/main/java/com/example/demo/TunehubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunehubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TunehubProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/repositories/PlaylistRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entities.Playlist;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, Integer> {
+
+	
+
+	
+
+}
